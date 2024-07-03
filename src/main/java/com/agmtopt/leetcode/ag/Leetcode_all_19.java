df1210b77db0a704a9ddc67a916cca677253beda
agmtopy <agmtopy@163.com>
2024-07-03 23:25:17 +0800

    默认变更列表

--- src/main/java/com/agmtopt/leetcode/ag/Leetcode_all_19.java
+++ src/main/java/com/agmtopt/leetcode/ag/Leetcode_all_19.java
@@ -1,25 +1,47 @@
 package com.agmtopt.leetcode.ag;
 
+import com.agmtopt.leetcode.ag.domain.ListNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Leetcode_all_19 {
 
     public static void main(String[] args) {
+        ListNode head = constructListNode1();
 
+        Solution solution = new Solution();
+        solution.removeNthFromEnd(head,2);
 
+        System.out.println(head);
      }
 
-    /**
-     * Definition for singly-linked list.
-     * public class ListNode {
-     *     int val;
-     *     ListNode next;
-     *     ListNode() {}
-     *     ListNode(int val) { this.val = val; }
-     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-     * }
-     */
-    class Solution {
-        public ListNode removeNthFromEnd(ListNoGde head, int n) {
+    private static ListNode constructListNode1() {
+        ListNode node5 = new ListNode(5,null);
+        ListNode node4 = new ListNode(4,node5);
+        ListNode node3 = new ListNode(3,node4);
+        ListNode node2 = new ListNode(2,node3);
+        ListNode node1 = new ListNode(1,node2);
+        return node1;
+    }
 
+    /**
+     * 删除链表中的指定的后第N个元素
+     */
+    static class Solution {
+        public ListNode removeNthFromEnd(ListNode head, int n) {
+            List<ListNode> ls = new ArrayList<>();
+
+            ListNode tempNode = head;
+            while (null != tempNode){
+                ls.add(tempNode);
+                tempNode = tempNode.next;
+            }
+
+            ls.get(ls.size() - n - 1).next = ls.get(ls.size() - n).next;
+            ls.get(ls.size() - n).next = null;
+
+            return head;
         }
     }
 }
